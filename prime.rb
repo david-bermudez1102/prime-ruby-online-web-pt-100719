32eb52d802385fb961a7a080b3d9f0238e809f13
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 14:41:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(number)
   number = number.abs
   if(number!=0 || number!=1)
     temp_array = (0..number).to_a - [0]
-    temp_array.each { |e|
-        puts number%e
+    is_a_prime = temp_array.all? { |e|
+        number%e == 0
     }
   else
     false
