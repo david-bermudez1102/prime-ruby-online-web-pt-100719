993423badf10c223066d6cd46de4866c452f7863
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 14:10:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   temp_array = (-(number)..number).to_a
   is_a_prime = temp_array.any? { |e|
     if(e!=0)
-      number%e == 0
+      number%e == 0 && number!=1
     end
   }
   is_a_prime
