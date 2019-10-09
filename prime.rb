71f915d8e02cfdc8c239c2b69f8717dd0d21b39c
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 15:00:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,9 @@ def prime?(number)
     temp_array = (0..number).to_a - [0]
     counter = 0
     temp_array.each { |e|
-        if(number%e == 0)
-          counter += 1
-        end
+      if(number%e == 0)
+        counter += 1
+      end
     }
     if(counter == 2)
       true
